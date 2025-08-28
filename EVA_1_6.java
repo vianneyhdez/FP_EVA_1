06d1b6d2563b10ff6be21f51ee0603faafec26bd
vianneyhdez <vianneyy00h@gmail.com>
2025-08-28 17:55:06 -0600

    Add files via upload

---
+++ EVA_1_6.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_6;
+
+/**
+ *
+ * @author Diego
+ */
+public class EVA_1_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //AREA DE UN CIRCULO
+        //DECLARACION VARIABLE
+        double area;
+        double radio;
+        double pi;
+        //INICIALIZACION
+        pi = 3.1416;
+        radio = 5;
+        area = pi * radio * radio;
+        //IMPRIMIR
+        System.out.println ("El valor de un circulo de radio es:");
+        System.out.println (area);
+        
+        
+       
+         }
+    
+}
---
+++ EVA_1_7_TRIANGULO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_7_triangulo;
+
+/**
+ *
+ * @author Diego
+ */
+public class EVA_1_7_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //AREA DE UN TRIANGULO 
+        //AREA = (BASE X ALTURA) ENTRE 2:
+        double area,base,altura;//tres variables  de tipo double
+        //INICIALIZACION
+        base = 10;
+        altura = 8;
+        area = (base * altura)/ 2.0; //CUIDADO CON DIVIDIR ENTRE ENTEROS
+        System.out.println("El area del triangulo base 10 y altura 8");
+        System.out.println(area);
+        
+        
+    }
+    
+}
